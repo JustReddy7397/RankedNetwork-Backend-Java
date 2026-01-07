a6af2d4fefb42b469cda95d551d9d886265e531c
JustReddy7397 <novoiceover2@gmail.com>
2026-01-07 14:53:15 +0100

    Add redis data to application.properties

--- src/main/java/network/ranked/backend/config/RedisConfig.java
+++ src/main/java/network/ranked/backend/config/RedisConfig.java
@@ -1,8 +1,15 @@
 package network.ranked.backend.config;
 
+import io.lettuce.core.ClientOptions;
+import io.lettuce.core.SslOptions;
+import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
@@ -12,9 +19,46 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 @Configuration
 public class RedisConfig {
 
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    @Value("${spring.data.redis.password}")
+    private String password;
+
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        final RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration(
+                host,
+                port
+        );
+
+        if (password != null && !password.isBlank()) {
+            configuration.setPassword(password);
+        }
+
+        // This is insecure. I know, don't care, everything is localhosted for me anyway
+        final SslOptions sslOptions = SslOptions.builder().
+                trustManager(InsecureTrustManagerFactory.INSTANCE).build();
+
+        final ClientOptions clientOptions = ClientOptions.builder()
+                .sslOptions(sslOptions)
+                .build();
+
+        final LettuceClientConfiguration clientConfig =
+                LettuceClientConfiguration.builder()
+                        .clientOptions(clientOptions)
+                        .useSsl()
+                        .build();
+
+        return new LettuceConnectionFactory(configuration, clientConfig);
+    }
+
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        final RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
         return template;
     }
